9edd827d494b6075feede680b1fbcd1da35fd040
Michael Cheng Chi Mun <MICHAELCCM@SINGAPOREPOWER.COM.SG>
2017-05-21 12:46:16 +0800

    Use appropriate syntax for bash.

--- gocd.sh
+++ gocd.sh
@@ -5,7 +5,7 @@ export PATH=$PATH:$GOPATH/bin
 
 env
 
-cd src/${GOCD_GODOMAIN:?"github.com"}/${GOCD_GOORG:?"SingaporePower"}/${GOCD_GOPACKAGE:?"gocd-go-sample"}
+cd src/"${GOCD_GODOMAIN:=github.com}"/"${GOCD_GOORG:=SingaporePower}"/"${GOCD_GOPACKAGE:=gocd-go-sample}"
 
 go get github.com/onsi/ginkgo/ginkgo
 go get github.com/onsi/gomega
